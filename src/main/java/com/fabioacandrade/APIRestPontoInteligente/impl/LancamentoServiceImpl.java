9edb5c7813560bd0fca72391c7dfddf6af96527e
Fábio Antônio Caldeira Andrade <fabio.andrade.1452236@sga.pucminas.br>
2024-05-20 15:16:15 -0300

    Add EhCache

--- src/main/java/com/fabioacandrade/APIRestPontoInteligente/impl/LancamentoServiceImpl.java
+++ src/main/java/com/fabioacandrade/APIRestPontoInteligente/impl/LancamentoServiceImpl.java
@@ -6,6 +6,8 @@ import com.fabioacandrade.APIRestPontoInteligente.services.LancamentoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -25,11 +27,13 @@ public class LancamentoServiceImpl implements LancamentoService {
         return this.lancamentoRepository.findByFuncionarioId(funcionarioId, pageRequest);
     }
 
+    @Cacheable("lancamentoPorId")
     public Optional<Lancamento> buscarPorId(Long id) {
         log.info("Buscando um lançamento pelo id {}",id);
         return this.lancamentoRepository.findById(id);
     }
 
+    @CachePut("lancamentoPorId")
     public Lancamento persistir(Lancamento lancamento) {
         log.info("Persistindo pelo lançamento {}", lancamento);
         return this.lancamentoRepository.save(lancamento);
